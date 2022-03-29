0ca6128e76f24ef77966fda30fc4331ab1cc8d76
Xiaoxiao <wangxiaoxiang@outlook.com>
2022-03-30 00:52:25 +0800

    init Memorize

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 汪潇翔 on 2022/3/30.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
